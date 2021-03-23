9d813dcbceca885b51f14d48196ac4e55ad84dc8
MerveKardes <mmervekardess@gmail.com>
2021-03-23 16:56:24 +0300

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserOperationClaims] (
+    [Id]               INT IDENTITY (1, 1) NOT NULL,
+    [UserId]           INT NOT NULL,
+    [OperationClaimId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+	FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
+    FOREIGN KEY ([OperationClaimId]) REFERENCES [dbo].[OperationClaims] ([Id])
+);
